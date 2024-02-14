39d2b31f57ac37a5d911d27dd87736197a58680c
DoneRus <bogdan.bunea07@gmail.com>
2024-02-14 11:32:50 +0100

    DIT change

--- javascript/value.js
+++ javascript/value.js
@@ -1,4 +1,19 @@
 var score = 0;
+
+var coinCost = 50;
+var DIT = 0;
+
+function buyCoins() {
+  if (score >= coinCost){
+  score = score - coinCost;
+  DIT = DIT + 1;
+  coinCost =  Math.round(coinCost * 1.25);
+  document.getElementById("coincost").innerHTML = coinCost; 
+  document.getElementById("score").innerHTML = score + "D"; 
+  document.getElementById("DIT").innerHTML = DIT; 
+  }
+}
+
 function add() {
   score++;
   document.getElementById("score").innerHTML = score + "D"; 
